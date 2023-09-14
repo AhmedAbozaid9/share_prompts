cca20bcf03c3c86023dd8b2aafe2ad771e93fd3e
ahmed <ahmedabozaid493@gmail.com>
2023-09-14 21:15:42 +0300

    adds the feed and promptCard

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,48 @@
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
+// a helper component
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div>
+      {data?.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 const Feed = () => {
-    return (
-        <div>
-            Feed
-        </div>
-    );
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState();
+  const handleSearchChange = (e) => {};
+  //get the data
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    })();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          value={searchText}
+          onChange={handleSearchChange}
+          placeholder="Search for a tag or a username"
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} />
+    </section>
+  );
 };
 
 export default Feed;
