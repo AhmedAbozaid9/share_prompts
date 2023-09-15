874a30fb493942f0edfcdf8814b8a2f6dc6ea12c
ahmed <ahmedabozaid493@gmail.com>
2023-09-15 16:13:35 +0300

    finishes the basic projects req

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
 import Profile from "@components/Profile";
+import { useRouter } from "next/navigation";
 
 const MyProfile = () => {
   const { data: session } = useSession();
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState([]);
+  const router = useRouter();
   useEffect(() => {
     (async () => {
       if (session?.user.id) {
@@ -17,8 +19,29 @@ const MyProfile = () => {
     })();
   }, []);
 
-  const handleEdit = () => {};
-  const handleDelete = () => {};
+  const handleEdit = async (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?",
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <Profile
       name="My"
